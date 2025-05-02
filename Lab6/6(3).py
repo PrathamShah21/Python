281c8244250abd052f187ff247798ed34839ec37
PrathamShah21 <shahpratham31005@gmail.com>
2025-05-02 11:21:33 +0530

    Delete Lab6 directory

--- Lab6/6(3).py
+++
@@ -1,8 +0,0 @@
-from datetime import date
-d1 = (3,10,2005)
-d2 = (1,8,2006)
-date1 = date(d1[2],d1[1],d1[0])
-date2 = date(d2[2],d2[1],d2[0])
-datediff = abs(date2-date1)
-print('Difference between date',date1,'&',date2,'is',datediff)
-
